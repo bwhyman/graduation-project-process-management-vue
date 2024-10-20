580388c385ade11dadcf5695456426c70ea2fa50
BO <bwang0629@gmail.com>
2024-10-21 01:09:05 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -26,6 +26,8 @@ declare module 'vue' {
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
     ElText: typeof import('element-plus/es')['ElText']
     LoadingVue: typeof import('./src/components/LoadingVue.vue')['default']
--- src/views/main/teacher/functions/GroupingView.vue
+++ src/views/main/teacher/functions/GroupingView.vue
@@ -3,6 +3,7 @@ import { createElNotificationSuccess } from '@/components/message'
 import { TeacherService } from '@/services/TeacherService'
 import type { Student, User } from '@/types'
 import { Check } from '@element-plus/icons-vue'
+import type { TabsPaneContext } from 'element-plus'
 import { exportGroup } from './GroupingView'
 
 const result = await Promise.all([
@@ -17,26 +18,40 @@ const disbR = ref(0)
 let groupNumber = -1
 
 // 新分组
-const newStuGroupMap = ref(new Map<number, { students: User[]; total: number }>())
+const newStuGroupMap = ref(
+  new Map<number, { students: User[]; total: number; teacherCount: Map<string, number> }>()
+)
+//
+const currentGroupStudentsR = ref<{
+  students: User[]
+  total: number
+  teacherCount: Map<string, number>
+}>()
+//
+const activeNameR = ref('0')
+
+// 学生按导师分组
+const teacherGroupMap = new Map<number, User[]>()
+teachersR.value.forEach((t) => (groupNumber = Math.max(groupNumber, t.groupNumber ?? 0)))
+for (let index = 1; index <= groupNumber; index++) {
+  teacherGroupMap.set(index, [])
+  newStuGroupMap.value.set(index, {
+    students: [],
+    total: 0,
+    teacherCount: new Map<string, number>()
+  })
+}
+studentsR.value.forEach((st) => {
+  const tid = st.student?.teacherId
+  // 当前学生教师所在组
+  const teacherGroup = teachersR.value.find((teach) => teach.id == tid)?.groupNumber ?? 0
+  teacherGroupMap.get(teacherGroup)?.push(st)
+})
 
 const grouping = () => {
   groupMessageR.value = []
   disbR.value = 0
   newStuGroupMap.value.clear()
-  // 学生按导师分组
-  const teacherGroupMap = new Map<number, User[]>()
-  teachersR.value.forEach((t) => (groupNumber = Math.max(groupNumber, t.groupNumber ?? 0)))
-  for (let index = 1; index <= groupNumber; index++) {
-    teacherGroupMap.set(index, [])
-    newStuGroupMap.value.set(index, { students: [], total: 0 })
-  }
-
-  studentsR.value.forEach((st) => {
-    const tid = st.student?.teacherId
-    // 当前学生教师所在组
-    const teacherGroup = teachersR.value.find((teach) => teach.id == tid)?.groupNumber ?? 0
-    teacherGroupMap.get(teacherGroup)?.push(st)
-  })
 
   // 初始化
   for (let index = 1; index <= groupNumber; index++) {
@@ -54,7 +69,11 @@ const grouping = () => {
       })
       groupTotalTemp = studentsR.value.length - t
     }
-    newStuGroupMap.value.set(index, { students: [], total: groupTotalTemp })
+    newStuGroupMap.value.set(index, {
+      students: [],
+      total: groupTotalTemp,
+      teacherCount: new Map<string, number>()
+    })
   }
 
   //
@@ -131,6 +150,22 @@ const grouping = () => {
     disbR.value += len
     groupMessageR.value.push({ group: i, length: len })
   }
+
+  newStuGroupMap.value.forEach((stuG) => {
+    stuG.students.forEach((stu) => {
+      const teachName = stu.student?.teacherName
+      if (!teachName) return
+      let count = stuG.teacherCount.get(teachName)
+      if (!count) {
+        stuG.teacherCount.set(teachName, 1)
+      } else {
+        stuG.teacherCount.set(teachName, ++count)
+      }
+    })
+  })
+
+  activeNameR.value = '0'
+  currentGroupStudentsR.value?.students && (currentGroupStudentsR.value.students = [])
 }
 
 // -----------
@@ -150,6 +185,12 @@ const updateGroupF = async () => {
 const exportGroupF = async () => {
   const students = await TeacherService.listStudentsService()
   exportGroup(students.value)
+}
+//
+
+const handleClick = (tab: TabsPaneContext) => {
+  const index = Number.parseInt(tab.index!) + 1
+  currentGroupStudentsR.value = newStuGroupMap.value.get(index)
 }
 </script>
 <template>
@@ -175,6 +216,25 @@ const exportGroupF = async () => {
           第{{ temp.group ?? 0 }}组: {{ temp.length ?? 0 }};
         </span>
       </p>
+    </el-col>
+    <!--  -->
+    <el-col class="my-col">
+      <el-tabs v-model="activeNameR" @tab-click="handleClick">
+        <el-tab-pane
+          v-for="(group, index) of newStuGroupMap"
+          :label="`第${index + 1}组`"
+          :name="index"
+          :key="index">
+          <template v-for="(tc, index3) of currentGroupStudentsR?.teacherCount" :key="index3">
+            {{ tc }}
+          </template>
+          <br />
+          <template v-for="(student, index2) of currentGroupStudentsR?.students" :key="index2">
+            {{ index2 + 1 }} - {{ student.name }} - {{ student.student?.teacherName }}
+            <br />
+          </template>
+        </el-tab-pane>
+      </el-tabs>
     </el-col>
   </el-row>
 </template>
